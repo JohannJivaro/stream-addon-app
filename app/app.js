9c9aa33b587edd298ec4250c3892f9ba8a1fe3f7
Haukur Mar Bodvarsson <haukur@jivaro.com>
2016-02-23 14:29:58 +0000

    Working prototype

---
+++ app/app.js
@@ -0,0 +1,69 @@
+(function () {
+	'use strict';
+
+	/**
+	 * Angular module
+	 * WebStorm names: ngmodule
+	 * Recommended naming conventions:
+	 * moduleName = lowercase,
+	 */
+
+	/**
+	 * app module
+	 */
+	angular
+		.module('app', [])
+		.config(Config);
+
+	// Config
+	// --------------------------------------------------
+	/* @ngInject */
+	function Config() {
+
+	}
+
+	/**
+	 * Tourney Controller
+	 */
+	angular
+		.module('app')
+		.controller('TourneyCtrl', TourneyCtrl);
+
+	/* @ngInject */
+	function TourneyCtrl($http, $interval) {
+		// "Controller as" the ViewModel
+		var vm = this;
+
+		// Public ViewModel
+		// --------------------------------------------------
+		vm.data = [];
+
+		// Run
+		// --------------------------------------------------
+		activate();
+
+		// Private functions
+		// --------------------------------------------------
+		function activate() {
+			updateHtml();
+
+			setDataWatcher();
+		}
+
+		function updateHtml() {
+			$http.get('data/dummy.json')
+				.then(function successCallback(response) {
+					vm.data = response.data;
+				}, function errorCallback(response) {
+					console.log('error', response);
+				});
+		}
+
+		function setDataWatcher() {
+			$interval(function() {
+				updateHtml();
+			}, 5000)
+		}
+	}
+
+})();
