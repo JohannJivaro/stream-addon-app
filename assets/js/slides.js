c5283e1a28571f9676153d981f19698617fdf2d6
Jóhann Schröder <johann@jivaro.com>
2017-01-26 10:13:11 +0000

    fixed slides.js

--- assets/js/slides.js
+++ assets/js/slides.js
@@ -6,7 +6,7 @@ $(function () {
     $("#slideshow > div:gt(0)").hide();
 
     setInterval(function () {
-        $('#slideshow > div:first')s
+        $('#slideshow > div:first')
             .fadeOut(3000)
             .next()
             .fadeIn(3000)
